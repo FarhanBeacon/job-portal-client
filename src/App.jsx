26839f27c8fb99ac0ea6b75dbd15f7384b962d6e
FarhanBeacon <farhanahmedbeacon@gmail.com>
2025-05-12 19:35:09 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl">My React App</h1>
+        <button className="btn px-8">Ok</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
